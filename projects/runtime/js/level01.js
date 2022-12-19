692a3d6f077976d73fdf944b771ca4b753bdb3a2
mylesborey12 <Borey220@stcharles.k12.la.us>
2022-12-19 14:21:47 +0000

    runtime

--- projects/runtime/js/level01.js
+++ projects/runtime/js/level01.js
@@ -5,7 +5,7 @@ var level01 = function (window) {
     var draw = window.opspark.draw;
     var createjs = window.createjs;
 
-    window.opspark.runLevelInGame = function(game) {
+    window.opspark.runLevelInGame = function (game) {
         // some useful constants 
         var groundY = game.groundY;
 
@@ -13,7 +13,7 @@ var level01 = function (window) {
         // behavior of our game
         var levelData = {
             "name": "Robot Romp",
-            "number": 1, 
+            "number": 1,
             "speed": -3,
             "gameItems": [
                 { "type": "sawblade", "x": 400, "y": groundY },
@@ -27,16 +27,51 @@ var level01 = function (window) {
 
         // TODO 6 and on go here
         // BEGIN EDITING YOUR CODE HERE
+        function createSawBlade(x, y) {
+            var hitZoneSize = 25;
 
-        
-        
-        
+            var damageFromObstacle = 10;
+
+            var sawBladeHitZone = game.createObstacle(hitZoneSize, damageFromObstacle);
+
+            sawBladeHitZone.x = x;
+
+            sawBladeHitZone.y = y;
+
+            game.addGameItem(sawBladeHitZone);
+
+            var obstacleImage = draw.bitmap("img/beam.png");
+            sawBladeHitZone.addChild(obstacleImage);
+
+            obstacleImage.x = -24
+            obstacleImage.y = -25
+
+        }
+
+        createSawBlade(400, 495);
+        createSawBlade(800, 490);
+        createSawBlade(1600, 577)
+
+
+        var enemy = game.createGameItem("enemy", 25);
+        var redSquare = draw.bitmap("img/enemy.png");
+        redSquare.scaleX = .5
+        redSquare.scaleY = .5
+        redSquare.x = -25;
+        redSquare.y = -25;
+        enemy.addChild(redSquare);
+
+        enemy.x = 400;
+        enemy.y = groundY - 140;
+
+        game.addGameItem(enemy);
+
         // DO NOT EDIT CODE BELOW HERE
     }
 };
 
 // DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
-if((typeof process !== 'undefined') &&
+if ((typeof process !== 'undefined') &&
     (typeof process.versions.node !== 'undefined')) {
     // here, export any references you need for tests //
     module.exports = level01;
--- projects/runtime/js/view/background.js
+++ projects/runtime/js/view/background.js
@@ -1,32 +1,31 @@
 var background = function (window) {
     'use strict';
-    
+
     window.opspark = window.opspark || {};
     var draw = window.opspark.draw;
     var createjs = window.createjs;
-    
+
     /*
      * Create a background view for our game application
      */
-    window.opspark.makeBackground = function(app,ground) {
+    window.opspark.makeBackground = function (app, ground) {
         /* Error Checking - DO NOT DELETE */
-        if(!app) {
+        if (!app) {
             throw new Error("Invalid app argument");
         }
-        if(!ground || typeof(ground.y) == 'undefined') {
+        if (!ground || typeof (ground.y) == 'undefined') {
             throw new Error("Invalid ground argument");
         }
-        
+
         // useful variables
         var canvasWidth = app.canvas.width;
         var canvasHeight = app.canvas.height;
         var groundY = ground.y;
-        
+
         // container which will be returned
         var background;
-        
+
         // ANIMATION VARIABLES HERE:
-        
      
         // called at the start of game and whenever the page is resized
         // add objects for display in background. draws each image added to the background once
@@ -36,21 +35,45 @@ var background = function (window) {
             // TODO: 2 - Part 2
             // this fills the background with a obnoxious yellow
             // you should modify this to suit your game
-            var backgroundFill = draw.rect(canvasWidth,canvasHeight,'yellow');
+            var backgroundFill = draw.rect(canvasWidth, canvasHeight, 'white');
             background.addChild(backgroundFill);
-            
+
             // TODO: 3 - Add a moon and starfield
-            
-            
-            // TODO 5: Part 1 - Add buildings!     Q: This is before TODO 4 for a reason! Why?
-            
-            
+
+
+            for (var star = 0; star <= 100; star++) {
+                var circle = draw.bitmap("img/stars.png");
+                circle.x = canvasWidth * Math.random();
+                circle.y = groundY * Math.random();
+                background.addChild(circle);
+            }
+
+            var moon = draw.bitmap("img/moon.png");
+            moon.x = 1000;
+            moon.y = 10;
+            moon.scaleX = 1;
+            moon.scaleY = 1;
+            background.addChild(moon);
+
+            // TODO 5: Part 1 - Add buildings!     Q: This is before TODO 4 for a reason! Why?   
+            for (var i = 0; i < 1; ++i) {
+                var buildingHeight = 300;
+                var building = draw.bitmap('img/building.png');
+                building.x = 200 * i;
+                building.y = groundY - buildingHeight;
+                background.addChild(building);
+                buildings.push(building);
+              }
             // TODO 4: Part 1 - Add a tree
-            
-            
+            spaceship = draw.bitmap("img/ship.png");
+            spaceship.x = 500;
+            spaceship.y = 260
+            background.addChild(spaceship);
+
         } // end of render function - DO NOT DELETE
+        var spaceship;
         
-        
+        var buildings = [];
         // Perform background animation
         // called on each timer "tick" - 60 times per second
         function update() {
@@ -58,34 +81,42 @@ var background = function (window) {
             var canvasWidth = app.canvas.width;
             var canvasHeight = app.canvas.height;
             var groundY = ground.y;
-            
+
             // TODO 4: Part 2 - Move the tree!
-            
-            
-            // TODO 5: Part 2 - Parallax
-            
+            spaceship.x = spaceship.x - 2.5;
+            if (spaceship.x < -200) {
+            spaceship.x = canvasWidth;
+            }
+        // TODO 5: Part 2 - Parallax
+        for (var i = 0; i < buildings.length; i++) {
+            var eachElement = buildings[i];
+            eachElement.x = eachElement.x - 1.6; 
+            if(eachElement.x <= -200){
+                eachElement.x = canvasWidth;
+            }
+          }
 
         } // end of update function - DO NOT DELETE
-        
-        
-        
-        /* Make a createjs Container for the background and let it know about the render and upate functions*/
-        background = new createjs.Container();
-        background.resize = render;
-        background.update = update;
-        
-        /* make the background able to respond to resizing and timer updates*/
-        app.addResizeable(background);
-        app.addUpdateable(background);
-        
-        /* render and return the background */
-        render();
-        return background;
+
+
+
+    /* Make a createjs Container for the background and let it know about the render and upate functions*/
+    background = new createjs.Container();
+    background.resize = render;
+    background.update = update;
+
+    /* make the background able to respond to resizing and timer updates*/
+    app.addResizeable(background);
+    app.addUpdateable(background);
+
+    /* render and return the background */
+    render();
+    return background;
     };
 };
 
 // DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
-if((typeof process !== 'undefined') &&
+if ((typeof process !== 'undefined') &&
     (typeof process.versions.node !== 'undefined')) {
     // here, export any references you need for tests //
     module.exports = background;
